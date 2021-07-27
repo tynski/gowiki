783ea3768603191577434eba6a8c8c2529117f89
tynski <b.tynski@gmail.com>
2021-07-27 22:24:01 +0200

    main updated

--- wiki.go
+++ wiki.go
@@ -33,8 +33,6 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil)
 }
